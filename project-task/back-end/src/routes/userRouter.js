ff5da76efc0a262e57f752c62691595397446111
EduardGishyan <164676522+EduardGishyan1@users.noreply.github.com>
2025-03-04 23:38:43 +0400

    Update userRouter.js

--- project-task/back-end/src/routes/userRouter.js
+++ project-task/back-end/src/routes/userRouter.js
@@ -5,12 +5,14 @@ import IsAdmin from "../middlewares/checkRole.js"
 
 const userRouter = express.Router()
 
-userRouter.get("/", userController.seeUsers)
-userRouter.get("/:id",userController.seeUserByID)
+userRouter.use(checkToken)
+
+userRouter.get("/", IsAdmin,userController.seeUsers)
+userRouter.get("/:id",IsAdmin,userController.seeUserByID)
 userRouter.get("/profile/me",userController.getUserProfile)
 userRouter.put("/profile/me",userController.updateUserProfile)
 userRouter.delete("/profile/me",userController.deleteUserProfile)
-userRouter.delete("/:id",userController.deleteUserById)
-userRouter.put("/:id",userController.updateUserById)
+userRouter.delete("/:id",IsAdmin,userController.deleteUserById)
+userRouter.put("/:id",IsAdmin,userController.updateUserById)
 
 export default userRouter
