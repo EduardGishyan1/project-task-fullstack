70a2fd2af14922a8873e10fc46f999317a53a6a5
EduardGishyan <164676522+EduardGishyan1@users.noreply.github.com>
2025-03-04 23:40:10 +0400

    Update authRouter.js

--- project-task/back-end/src/routes/authRouter.js
+++ project-task/back-end/src/routes/authRouter.js
@@ -6,7 +6,6 @@ const authRouter = express.Router()
 authRouter.use(express.json());
 authRouter.use(express.urlencoded({ extended: true })); 
 
-
 authRouter.post("/register", authController.registerUser)
 authRouter.post("/login", authController.loginUser)
 authRouter.get("/logout", checkToken, authController.logoutUser)
